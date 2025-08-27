de2d76f8409298c923e1d2a917bac5f4fd2387e8
dvforest <dvforest@hotmail.com>
2025-08-26 21:23:10 -0400

    Import icons and add custom chevron for select box

---
+++ src/assets/icons.js
@@ -0,0 +1,9 @@
+import downSVG from './icons/chevron-down.svg';
+import checkSVG from './icons/check-circle.svg';
+import invalidSVG from './icons/close-circle.svg';
+
+export const icon = {
+    down: downSVG,
+    check: checkSVG,
+    invalid: invalidSVG,
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "./style.css";
+import './style.css';
+import { icon } from './assets/icons.js';
+
+// Use custom chevron icon for select box to adjust positoning via css
+const select = document.querySelector('.input-select');
+select.style.backgroundImage = `url(${icon.down})`;
