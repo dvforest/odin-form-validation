e282c528e96563e0f3e8473c791c1ab235df14c9
dvforest <dvforest@hotmail.com>
2025-08-28 10:56:54 -0400

    Add checkmarks after each form inpouts

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import { icon } from './assets/icons.js';
 // Use custom chevron icon for select box to adjust positoning via css
 const select = document.querySelector('.input-select');
 select.style.backgroundImage = `url(${icon.down})`;
+
+// Assign correct images to each validation check
+const checks = document.querySelectorAll('.input-check');
+checks.forEach((check) => (check.src = icon.check));
